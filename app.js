a4febf8eb1359dc680c724b636a6815e8b3387ea
abitshrestha <abitshrestha2079@gmail.com>
2023-12-02 12:00:28 +0545

    logout

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname,'public')));
-app.use(cookieParser());
+app.use(cookieParser());    
 
 app.use('/',authRouter);
 app.use('/',fruitRouter);
